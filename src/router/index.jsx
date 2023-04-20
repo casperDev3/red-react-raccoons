f39818125c64c96449a480a11c7099ad015d6429
zerodott <93039953+casperDev3@users.noreply.github.com>
2023-04-20 20:06:44 +0300

    add lection

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Home from "../pages/home/Home";
-import Converter from "../pages/converter/Converter";
 import { Route, Routes } from "react-router-dom";
+
+// custom pages
+import Converter from "../pages/converter/Converter";
+import JSX from "../pages/jsx/learning";
 
 const RouterApp = () => {
   return (
@@ -11,6 +14,7 @@ const RouterApp = () => {
       <Route exact path="/posts" element={<Posts />}></Route> */}
       <Route path="/" element={<Home />}></Route>
       <Route path="/converter" element={<Converter />}></Route>
+      <Route path="/jsx" element={<JSX />}></Route>
     </Routes>
   );
 };
